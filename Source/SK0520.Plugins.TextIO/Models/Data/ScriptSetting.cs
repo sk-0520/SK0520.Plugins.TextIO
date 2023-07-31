9b3095e44b17a85bac608e425e5e9b31ff676831
sk <content.type.text.net@gmail.com>
2023-07-31 19:28:01 +0900

    はぁ

--- Source/SK0520.Plugins.TextIO/Models/Data/ScriptSetting.cs
+++ Source/SK0520.Plugins.TextIO/Models/Data/ScriptSetting.cs
@@ -6,9 +6,30 @@ using System.Threading.Tasks;
 
 namespace SK0520.Plugins.TextIO.Models.Data
 {
-    public record ScriptSetting(
+    public record ScriptHeadSetting(
+        Guid ScriptId,
         string Name,
-        List<ScriptParameter> Parameters,
+        List<ScriptParameter> Parameters
+    );
+
+    public record ScriptBodySetting(
+        Guid ScriptId,
         string Source
     );
+
+    public record ScriptSetting {
+        public ScriptSetting(ScriptHeadSetting head,ScriptBodySetting body)
+        {
+            if(head.ScriptId != body.ScriptId)
+            {
+                throw new ArgumentException(nameof(body.ScriptId));
+            }
+
+            Head = head;
+            Body = body;
+        }
+
+        public ScriptHeadSetting Head { get; }
+        public ScriptBodySetting Body { get; }
+    }
 }
