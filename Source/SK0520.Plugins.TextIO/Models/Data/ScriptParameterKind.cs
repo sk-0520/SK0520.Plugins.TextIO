4865cee0dd0c7925f6cc37d1172f2f9dd37616df
sk <content.type.text.net@gmail.com>
2023-07-31 18:49:23 +0900

    setting

---
+++ Source/SK0520.Plugins.TextIO/Models/Data/ScriptParameterKind.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SK0520.Plugins.TextIO.Models.Data
+{
+    public enum ScriptParameterKind
+    {
+        Unknown,
+        String,
+        Integer,
+        Decimal,
+        DateTime,
+    }
+}
---
+++ Source/SK0520.Plugins.TextIO/Models/Data/ScriptSetting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SK0520.Plugins.TextIO.Models.Data
+{
+    public class ScriptSetting
+    {
+        #region property
+
+        public string Name { get; set; } = string.Empty;
+
+        public Dictionary<string, ScriptParameterKind> Parameters { get; set; } = new Dictionary<string, ScriptParameterKind>();
+
+        public string Source { get; set; } = string.Empty;
+
+        #endregion
+    }
+}
