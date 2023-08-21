4823030e50ec6663ab8327b376034c1b3f39c75c
sk <content.type.text.net@gmail.com>
2023-08-21 23:21:58 +0900

    いらぬ

--- Source/SK0520.Plugins.TextIO/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using ContentTypeTextNet.Pe.Bridge.Plugin;
-using ContentTypeTextNet.Pe.Embedded.Attributes;
-
-[assembly: PluginIdentifiers("SK0520.Plugins.TextIO", "d18d1b03-549d-4753-8cbc-f8b230131ec2")]
-[assembly: PluginSupportVersions("0.0.0", "0.0.0", "<https://example.com/update-check.json?")]
-[assembly: PluginAuthors("<author>", PluginLicense.Unknown, "<website>", "<projectsite>", "<email>")]
