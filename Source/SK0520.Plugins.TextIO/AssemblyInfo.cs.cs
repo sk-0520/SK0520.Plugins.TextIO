e50453af2bb549ba8b92777c0989d8620ade116c
sk <content.type.text.net@gmail.com>
2023-09-02 17:11:02 +0900

    こまいの

--- Source/SK0520.Plugins.TextIO/AssemblyInfo.cs.cs
+++ Source/SK0520.Plugins.TextIO/AssemblyInfo.cs.cs
@@ -5,5 +5,5 @@ using ContentTypeTextNet.Pe.Bridge.Plugin;
 using ContentTypeTextNet.Pe.Embedded.Attributes;
 
 [assembly: PluginIdentifiers("SK0520.Plugins.TextIO", "d18d1b03-549d-4753-8cbc-f8b230131ec2")]
-[assembly: PluginSupportVersions("0.99.210", "0.0.0", "<https://example.com/update-check.json?")]
-[assembly: PluginAuthors("<author>", PluginLicense.Unknown, "<website>", "<projectsite>", "<email>")]
+[assembly: PluginSupportVersions("0.99.210", "0.0.0", "https://github.com/sk-0520/SK0520.Plugins.TextIO/releases/latest/download/update-SK0520.Plugins.TextIO.json")]
+[assembly: PluginAuthors("<author>", PluginLicense.DoWhatTheF_ckYouWantToPublicLicense2, "<website>", "https://github.com/sk-0520/SK0520.Plugins.TextIO", "<email>")]
